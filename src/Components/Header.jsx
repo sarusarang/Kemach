df496e22edca33e8c60bd0047fa476e5a36c26e8
sarusarang <sarangsaru445@gmail.com>
2024-09-23 17:18:27 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -90,11 +90,11 @@ function Header() {
 
                                         <li><Link to={'/skidloader'} className="dropdown-item" >Skid-Steer Loaders</Link></li>
                                         <li><Link to={'/mini'} className="dropdown-item" >Mini Excavators</Link></li>
-                                        <li><Link className="dropdown-item" >Backhoe Loaders</Link></li>
-                                        <li><Link className="dropdown-item" >Rock Breakers </Link></li>
-                                        <li><Link className="dropdown-item" >Hydraulic Attachments & Drill Bits</Link></li>
-                                        <li><Link className="dropdown-item" >Electric Compressors</Link></li>
-                                        <li><Link className="dropdown-item" >Service & Parts </Link></li>
+                                        <li><Link to={'/mini'} className="dropdown-item" >Backhoe Loaders</Link></li>
+                                        <li><Link to={'/mini'} className="dropdown-item" >Rock Breakers </Link></li>
+                                        <li><Link to={'/mini'} className="dropdown-item" >Hydraulic Attachments & Drill Bits</Link></li>
+                                        <li><Link to={'/mini'} className="dropdown-item" >Electric Compressors</Link></li>
+                                        <li><Link to={'/mini'} className="dropdown-item" >Service & Parts </Link></li>
 
                                     </ul>
 
